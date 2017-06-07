266d4438e9e47e48f90f0fa367467a4cbdd46b35
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-06-07 10:31:02 -0400

    adds function to change all tile colors if game is won

--- script.js
+++ script.js
@@ -12,13 +12,36 @@ var tiles = document.getElementsByClassName("tile");
 var winningColor = colors[1];
 var winningColorDisplay = document.getElementById("winning-color");
 
+var winningColorDisplay = document.getElementById("winning-color");
+winningColorDisplay.textContent = winningColor;
+
+var message = document.getElementById("message");
+
 for(var i = 0; i < tiles.length; i ++) {
 		tiles[i].style.backgroundColor = colors[i];
+
 		tiles[i].addEventListener("click", function(){
-			alert("clicked");
+			var clickedColor = this.style.backgroundColor;
+
+			if(clickedColor === winningColor) {
+				for(var i = 0; i < tiles.length; i ++) {
+					changeColors(clickedColor);
+					message.textContent = "Correct!";
+				}
+			} else {
+				for(var i = 0; i < tiles.length; i ++) {
+					this.style.backgroundColor = "#232323";
+					message.textContent = "Try Again!"
+				}
+			}
 		});
 };
 
 var winningColorDisplay = document.getElementById("winning-color");
 winningColorDisplay.textContent = winningColor;
 
+function changeColors(color) {
+	for(var i = 0; i < tiles.length; i ++) {
+		tiles[i].style.backgroundColor = color;
+	}
+};
