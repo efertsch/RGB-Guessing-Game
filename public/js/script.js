d4cae5b5817788b8f97c3df5b5b8afb0f6e2d0b6
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-12-06 17:53:23 -0500

    adds comments javascript code

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,38 @@
 // Variable Declarations 
-var colors = [];
-var tiles = document.getElementsByClassName("tile");
-var winningColor;
-var numOfTiles = 6;
-
-// Selectors
-var winningColorDisplay = document.getElementById("winning-color");
-var winningColorDisplay = document.getElementById("winning-color");
-var message = document.getElementById("message");
-var h1 = document.querySelector("h1");
-var buttons = document.querySelectorAll("button");
-var resetButton = document.getElementById("reset");
-var modeButtons = document.getElementsByClassName("mode");
+//----------------------
+var colors 							= [],
+ 		tiles 							= document.getElementsByClassName("tile"),
+ 		winningColor,
+ 		numOfTiles 					= 6,
+		winningColorDisplay = document.getElementById("winning-color"),
+	 	message 						= document.getElementById("message"),
+	 	h1 									= document.querySelector("h1"),
+	 	buttons 						= document.querySelectorAll("button"),
+	 	resetButton 				= document.getElementById("reset"),
+	 	modeButtons 				= document.getElementsByClassName("mode")
 
 
-// Functions
+// Helper functions
+//-----------------
+//Changes the background color of all tiles
 function changeColors(color) {
 	for(var i = 0; i < tiles.length; i ++) {
 		tiles[i].style.backgroundColor = color;
 	}
 };
 
+//Selects a random color in the colors array to be the winning color
 function pickWinningColor(){
-	var random = Math.floor(Math.random() * colors.length);
-	return colors[random];
+	var randomColor = Math.floor(Math.random() * colors.length);
+	return colors[randomColor];
 };
 
+// Sets text content of a given element
 function setContent(element, text){
 	element.textContent = text;
 };
 
+// Generates an array of random colors 
 function generateRandomColors(num) {
 	var arr = [];
 	for(var i = 0; i < num; i ++) {
@@ -38,6 +41,7 @@ function generateRandomColors(num) {
 	return arr;
 };
 
+// Generates a single color using random rgb values 
 function pickRandomColor(){
 	var redValue = Math.floor(Math.random() * 256);
 	var greenValue = Math.floor(Math.random() * 256);
@@ -45,23 +49,26 @@ function pickRandomColor(){
 	return "rgb(" + redValue + ", " + greenValue + ", " + blueValue + ")";
 };
 
-function addSelectedClass(btn){
-	btn.classList.add("selected");
+// Adds the 'selected' class to a given element
+function addSelectedClass(elem){
+	elem.classList.add("selected");
 };
 
-function removeSelectedClass(btns){
-	for(var i = 0; i < btns.length; i ++){
-		btns[i].classList.remove("selected");
+// Removes the 'selected' class from a given element
+function removeSelectedClass(elems){
+	for(var i = 0; i < elems.length; i ++){
+		elems[i].classList.remove("selected");
 	}
 };
 
+// Resets the game 
 function resetGame(){
 	colors = generateRandomColors(numOfTiles);
 	winningColor = pickWinningColor();
 	winningColorDisplay.textContent = winningColor;
 	setContent(resetButton, "New Colors");
 	setContent(message, "");
-
+	
 	for(var i = 0; i < tiles.length; i ++) {
 		if (colors[i]) {
 			tiles[i].style.display = "block";
@@ -74,6 +81,7 @@ function resetGame(){
 	h1.style.background = "steelblue";
 };
 
+// Sets up easy and hard mode button functionality
 function setUpModeButtons(){
 	for (var i = 0; i < modeButtons.length; i++){
 	modeButtons[i].addEventListener("click", function(){
@@ -85,6 +93,8 @@ function setUpModeButtons(){
 	};
 };
 
+// Loops over tiles and adds an event listener to each tile
+//Determines if clicked tile was of the correct color
 function setUpTiles(){
 	for(var i = 0; i < tiles.length; i ++) {
 		tiles[i].addEventListener("click", function(){
@@ -106,22 +116,25 @@ function setUpTiles(){
 	};
 };
 
+// Resets game when the 'reset' button is clicked
 function resetButtonEvent(){
 	resetButton.addEventListener("click", function(){
 		resetGame();
 	});
 };
 
+// Initializes the game
 function initGame() {
 	setUpModeButtons();
 	setUpTiles();
 	resetGame();
+	resetButtonEvent();
 };
 
 
-// Function Calls
+// Runs the program
 initGame();
-resetButtonEvent();
+
 
 
 
